fe4939eb1e6b8058da6fd0c603c4808ae8f8fd06
cherryz21 <charansri21@gmail.com>
2019-12-16 13:49:28 -0500

    first commit

---
+++ src/Example1.java
@@ -0,0 +1,16 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="foo";
+		String s2="fooyt";
+		if(s1.equals(s2)){
+			System.out.println("strings equal");
+		}
+		else
+			System.out.println("not equal");
+
+	}
+
+}
